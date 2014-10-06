54bc601585309f44014ddd3c426aa40fc6f63c41
Brian Dooley <brian.dooley@feedhenry.com>
2014-10-06 17:33:18 +0100

    remove logtest

--- logtest.js
+++
@@ -1,7 +0,0 @@
-  var logger = require('./logger');
-  logger.debugLevel = 'error';
-
-  logger.log('info', 'Everything started properly.');
-  logger.log('warn', 'Running out of memory...');
-  logger.log('error', 'All is doomed');
-  
