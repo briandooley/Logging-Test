9eaa2e862ea84e28065d2558336885cfe13b728d
Brian Dooley <brian.dooley@feedhenry.com>
2014-10-06 17:27:42 +0100

    Add UTC comment

--- logger.js
+++ logger.js
@@ -1,10 +1,12 @@
-  util = require('util');
+// All timestamps displayed are in UTC.
+// If you wish to see the current time in UTC, go to http://www.worldtimeserver.com/current_time_in_UTC.aspx
+util = require('util');
 
-  var logger = exports;
-  logger.debugLevel = 'error';
-  logger.log = function(level, message) {
-    var levels = ['error', 'warn', 'info'];
-    if (levels.indexOf(level) >= levels.indexOf(logger.debugLevel) ) {
-      util.log(level + ': ' + message);
-    }
-  };
+var logger = exports;
+logger.debugLevel = 'error';
+logger.log = function(level, message) {
+  var levels = ['error', 'warn', 'info'];
+  if (levels.indexOf(level) >= levels.indexOf(logger.debugLevel) ) {
+    util.log(level + ': ' + message);
+  }
+};
